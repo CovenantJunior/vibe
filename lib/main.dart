e2d135ff6b7cb8d731d3e963f79f28345e4dbf84
CovenantJunior <covenanttope@gmail.com>
2024-04-24 16:32:31 +0100

    Yipee

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:vibe/layouts/home_page.dart';
+
+void main() {
+  runApp(const Vibe());
+}
 
 class Vibe extends StatefulWidget {
   const Vibe({super.key});
@@ -10,6 +15,9 @@ class Vibe extends StatefulWidget {
 class _VibeState extends State<Vibe> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
   }
 }
