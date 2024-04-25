4160cbf6c9b8b04c950938e2fad35d4e5647afa2
CovenantJunior <covenanttope@gmail.com>
2024-04-25 12:29:54 +0100

    Fix schema

--- lib/model/vibe_database.dart
+++ lib/model/vibe_database.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:vibe/model/vibe_preferences.dart';
 
 class VibeDatabase extends ChangeNotifier {
@@ -9,7 +10,7 @@ class VibeDatabase extends ChangeNotifier {
   static Future<void> initialize() async {
     final dir = await getApplicationDocumentsDirectory();
     isar = await Isar.open(
-        [VibePreferences],
+        [VibePreferencesSchema],
         directory: dir.path);
   }
 }
