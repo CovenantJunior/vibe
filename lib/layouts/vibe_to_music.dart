8d255e51180497ebbd815b785a493f9a33a4734b
CovenantJunior <covenanttope@gmail.com>
2024-04-29 20:31:33 +0100

    Taking shape

--- lib/layouts/vibe_to_music.dart
+++ lib/layouts/vibe_to_music.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:on_audio_query/on_audio_query.dart';
 import 'package:on_audio_query_platform_interface/src/models/song_model.dart';
 import 'package:vibe/components/vibe_cover.dart';
 
@@ -24,8 +25,15 @@ class _VibeToMusicState extends State<VibeToMusic> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           VibeCover(
-            child: Icon(Icons.music_note_outlined)
+            child: QueryArtworkWidget(
+              id: widget.song.id,
+              type: ArtworkType.AUDIO,
+              quality: 100,
+              artworkHeight: 200,
+              artworkWidth: 200,
+            )
           ),
+          const SizedBox(height: 20),
           Center(
             child: Text(
               widget.song.title,
