22a5f0083c85a2bfa6d506af9e47d145bcee320c
CovenantJunior <covenanttope@gmail.com>
2024-05-08 16:33:10 +0100

    Needs tweaking

--- lib/components/vibe_to_song.dart
+++ lib/components/vibe_to_song.dart
@@ -18,64 +18,138 @@ class VibeToSong extends StatefulWidget {
 class _VibeToSongState extends State<VibeToSong> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 0),
-              child: Column(
-                children: [
-                  VibeCover(
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(10),
-                      child: Padding(
-                        padding: const EdgeInsets.all(15),
-                        child: QueryArtworkWidget(
-                          id: widget.song.id,
-                          type: ArtworkType.AUDIO,
-                          quality: 100,
-                          artworkHeight: 250,
-                          artworkWidth: 250,
-                          artworkBorder: BorderRadius.circular(10),
-                          nullArtworkWidget: const Icon(
-                            Icons.library_music_outlined,
-                            size: 200,
-                            color: Color.fromARGB(255, 202, 202, 123),
+    int sec = widget.song.duration!;
+    int songDuration = Duration(milliseconds: sec).inSeconds;
+
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    VibeCover(
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Padding(
+                          padding: const EdgeInsets.all(15),
+                          child: QueryArtworkWidget(
+                            id: widget.song.id,
+                            type: ArtworkType.AUDIO,
+                            quality: 100,
+                            artworkHeight: 250,
+                            artworkWidth: 250,
+                            artworkBorder: BorderRadius.circular(10),
+                            nullArtworkWidget: const Icon(
+                              Icons.library_music_outlined,
+                              size: 200,
+                              color: Color.fromARGB(255, 202, 202, 123),
+                            ),
                           ),
                         ),
-                      ),
-                    )
-                  ),
-                  const SizedBox(height: 30),
-                  Center(
-                    child: Text(
-                      widget.song.title,
-                      textAlign: TextAlign.center,
-                      overflow: TextOverflow.fade,
-                      style: const TextStyle(
-                        fontFamily: 'Futura',
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500),
-                  )),
-                  Center(
-                    child: Text(
-                      widget.song.artist!,
-                      textAlign: TextAlign.center,
-                      style: const TextStyle(
-                        fontFamily: 'Futura', fontWeight: FontWeight.w400
-                      ),
-                  )),
-                ],
-              ).animate().scale().flipV(
-                duration: Duration(milliseconds: 400)
+                      )
+                    ),
+                    const SizedBox(height: 30),
+                    Center(
+                      child: Text(
+                        widget.song.title,
+                        textAlign: TextAlign.center,
+                        overflow: TextOverflow.fade,
+                        style: const TextStyle(
+                          fontFamily: 'Futura',
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500),
+                    )),
+                    Center(
+                      child: Text(
+                        widget.song.artist!,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontFamily: 'Futura', fontWeight: FontWeight.w400
+                        ),
+                    )),
+                  ],
+                ).animate().scale().flipV(
+                  duration: const Duration(milliseconds: 400)
+                ),
               ),
-            ),
-            
-            const SizedBox(height: 50),
-
-            // Controls
-
-          ],
-        );
+              
+              const SizedBox(height: 70),
+      
+              // Controls
+              Column(
+                children: [
+                  Slider(
+                    min: 0,
+                    max: 1,
+                    value: 0,
+                    activeColor: const Color.fromARGB(255, 202, 202, 123),
+                    thumbColor: const Color.fromARGB(255, 202, 202, 123),
+                    onChanged: (value) {
+                  
+                    }
+                  ),
+                  
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          "0:00",
+                          style: TextStyle(
+                            fontFamily: 'Futura',
+                          ),
+                        ),
+                        Text(
+                          "${songDuration / 60}:${songDuration % 60}",
+                          style: const TextStyle(
+                            fontFamily: 'Futura',
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconButton(
+                          onPressed: null,
+                          icon: Icon(
+                            Icons.skip_previous_outlined,
+                            size: 30,
+                            color: Color.fromARGB(255, 202, 202, 123),
+                          )
+                        ),
+                        IconButton(
+                          onPressed: null,
+                          icon: Icon(
+                            Icons.play_arrow_outlined,
+                            size: 30,
+                            color: Color.fromARGB(255, 202, 202, 123),
+                          )
+                        ),
+                        IconButton(
+                          onPressed: null,
+                          icon: Icon(
+                            Icons.skip_next_outlined,
+                            size: 30,
+                            color: Color.fromARGB(255, 202, 202, 123),
+                          )
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+    );
   }
 }
