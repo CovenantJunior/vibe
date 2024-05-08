d29b7993921a16c74421b7b3d6ce7ed0da624d48
CovenantJunior <covenanttope@gmail.com>
2024-05-08 18:00:14 +0100

    Changes

---
+++ lib/audioplayer/audioplayer.dart
@@ -0,0 +1,32 @@
+/*
+ List of all Audio Functions 
+*/
+
+class AudioPlayer {
+  
+  bool isPlaying = false;
+  
+  void playMusic(uri) {
+
+  }
+
+  void pauseMusic(uri) {
+
+  }
+
+  void nextMusic(uri) {
+
+  }
+
+  void previousMusic(uri) {
+
+  }
+
+  void deleteMusic(uri) {
+
+  }
+
+  void fetchMusicLyrics(title) {
+
+  }
+}
