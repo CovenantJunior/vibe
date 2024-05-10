80e4f5b1f11d257fc6b792b2b554c0f69403159b
CovenantJunior <covenanttope@gmail.com>
2024-05-10 19:38:54 +0100

    We'll be swithcing packages

--- lib/components/vibe_music_tile.dart
+++ lib/components/vibe_music_tile.dart
@@ -1,4 +1,5 @@
-import 'package:audioplayers/audioplayers.dart';
+// import 'package:audioplayers/audioplayers.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:on_audio_query/on_audio_query.dart';
@@ -109,14 +110,16 @@ class _VibeMusicTileState extends State<VibeMusicTile> {
                           ),
                         ),
                         onTap: () {
-                          // print(song.uri!);
-                          _audioPlayer.setReleaseMode(ReleaseMode.release);
+                          print(song.uri!);
+                          _audioPlayer.setUrl(song.uri!);
+                          _audioPlayer.play();
+                          /* _audioPlayer.setReleaseMode(ReleaseMode.release);
                           audioProvider.currentDuration = Duration.zero;
                           audioProvider.totalDuration = Duration(microseconds: song.duration!);
                           audioProvider.playMusic(song.id, song.uri, song.duration);
                           audioProvider.isPlaying = true;
                           audioProvider.resume = false;
-                          Navigator.push(context, MaterialPageRoute(builder: (context) => VibeToMusic(song: song)));
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => VibeToMusic(song: song))); */
                         },
                       ).animate().fadeIn().scale(
                         duration: const Duration(milliseconds: 300)
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,16 +5,20 @@
 import FlutterMacOS
 import Foundation
 
+import audio_session
 import audioplayers_darwin
 import device_info_plus
 import flutter_local_notifications
 import isar_flutter_libs
+import just_audio
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
+  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
